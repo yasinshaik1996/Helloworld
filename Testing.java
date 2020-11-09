d572dd5b20a6688e18b193328cc989f390474afd
mariam-hue <67871162+mariam-hue@users.noreply.github.com>
2020-11-09 23:52:26 +0530

    Testing with pole scm

--- Testing.java
+++ Testing.java
@@ -3,7 +3,7 @@ public static void main(String[] args){
 
 for(int i=1; i<=10; i++){
 
-System.out.println("Testing..."+i);
+System.out.println("Testing with poll scm build triggers when change is pushed..."+i);
 }
 }
 
